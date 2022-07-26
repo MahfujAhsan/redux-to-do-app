8f7ac2f31609e9bef1dd87de812ae1e27be055cd
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-07-26 17:40:29 +0600

    updated

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav>
-            <Link to="/" className='nav-link'>Home</Link>
             <Link to="/show-books" className='nav-link'>Show Books</Link>
             <Link to="/add-books" className='nav-link'>Add Books</Link>
         </nav>
