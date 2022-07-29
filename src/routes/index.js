87d7ce8d6b6400a23faf68f3b216ee5393b09ef9
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-07-29 21:03:27 +0600

    root updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,6 @@ import BooksView from '../features/books/BooksView';
 import EditBooks from '../features/books/EditBooks';
 import Navbar from '../layouts/Navbar';
 import Error from './pages/Error';
-import Home from './pages/Home';
 
 const Index = () => {
     return (
@@ -13,7 +12,7 @@ const Index = () => {
             <Navbar />
             <main>
                 <Routes>
-                    <Route path='/' element={<Home />} />
+                    <Route path='/' element={<BooksView />} />
                     <Route path='/show-books' element={<BooksView />} />
                     <Route path='/add-books' element={<AddBooks />} />
                     <Route path='/edit-book' element={<EditBooks />} />
